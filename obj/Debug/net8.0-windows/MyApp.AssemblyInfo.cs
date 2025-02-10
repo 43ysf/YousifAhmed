1d425a6efead8051394458d7e4ae74b2f0f39264
Yousif <76800385+43ysf@users.noreply.github.com>
2025-02-10 21:49:17 +0300

    Updates

--- obj/Debug/net8.0-windows/MyApp.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/MyApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fa13219c54c2df11e33b07dac61f0d795843ef56")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+186ce32e71c41762aac183829d8aafb2ca7cbdeb")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
