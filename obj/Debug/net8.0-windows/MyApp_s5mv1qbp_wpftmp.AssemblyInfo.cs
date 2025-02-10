b37f09030c1396df486eec07862ef85ae2d0512c
Yousif <76800385+43ysf@users.noreply.github.com>
2025-02-10 23:00:43 +0300

    Yousif Updates

--- obj/Debug/net8.0-windows/MyApp_s5mv1qbp_wpftmp.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/MyApp_s5mv1qbp_wpftmp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fba444c841c181596882ccac78df0718b5840b9a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fa13219c54c2df11e33b07dac61f0d795843ef56")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
